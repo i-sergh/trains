359bb595f0532c4e44d61b36d54b05f25ea763a4
strangy-strange <seripp28@gmail.com>
2022-07-04 17:54:56 +0300

    Подготовыили код к реализации интерфейса

--- rails.py
+++ rails.py
@@ -6,6 +6,39 @@ WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 cnv = np.zeros( (WINDOW_HEIGHT, WINDOW_WIDTH, 3),
                dtype=np.uint8() )
+for_mouse = np.zeros( (WINDOW_HEIGHT, WINDOW_WIDTH, 3),
+               dtype=np.uint8() )
+
+class Dot:
+    """
+    Класс точки  интерфейса
+    Должен магнититься к точке перекрестка
+    """
+    def __init__ (self, x, y):
+        # центр точки
+        self.x = x
+        self.y = y
+        # предположительно разница от точки до позиции мыши
+        self.dx = 0
+        self.dy = 0
+        # отображение точки
+        self.visible = False
+    
+    def visibility(self):
+        self.visible = not self.visible
+        
+    def draw(self):
+        pass
+    def destroy(self):
+        pass
+    
+    def set_cross(self,event, x,y, flags, param):
+        #"""
+        #    Должно по позиции мыши отрисовывать
+        #    но только одну точку
+        #"""
+        print(event, x,y, flags, param)
+
 
 
 class Cross:
@@ -58,15 +91,29 @@ class Cross:
                            
                            [False, True], # top right
                            [False, True], # right bottom
-                           [False, True],
-                           [False, True],
+                           [False, True], # bottom left
+                           [False, True], # left_top
 
-                           [False, False],
-                           [False, False],
-                           [False, False],
-                           [False, True]
-                           
+                           [False, True], # top left
+                           [False, True], # left bottom
+                           [False, True], # bottom right
+                           [False, True]  # right top
                         ]
+        self.paramWay[0][1] &= top + bottom
+        self.paramWay[1][1] &= top + bottom
+        self.paramWay[2][1] &= left + right
+        self.paramWay[3][1] &= left + right
+
+        self.paramWay[4][1] &= top_right + right_top
+        self.paramWay[5][1] &= right_bottom + bottom_right
+        self.paramWay[6][1] &= bottom_left + left_bottom
+        self.paramWay[7][1] &= left_top + top_left
+
+        self.paramWay[8][1] &= left_top + top_left
+        self.paramWay[9][1] &= bottom_left + left_bottom
+        self.paramWay[10][1] &= right_bottom + bottom_right
+        self.paramWay[11][1] &= top_right + right_top
+                                                                                                
     def draw(self):
         for i, zn in enumerate(self.nlist):
             if self.paramWay[i][1]:
@@ -88,14 +135,26 @@ class Cross:
                         cv2.ellipse(cnv, self.drawDots[i][0], (30,30), 0,
                                     self.drawDots[i][1], self.drawDots[i][2],
                                     (0, 0,255), 4 )
-                
-n = Cross(300, 300,
-          False, True, False, True,
-          True, True, True, True, 
-          False, False, False, False)
-n.draw()
+
+n = []
+for x in range(60, WINDOW_WIDTH-30, 60):
+    for y in range(60, WINDOW_HEIGHT-30, 60):  
+         n.append(
+             Cross(x, y,
+          randint(0,1), randint(0,1), randint(0,1), randint(0,1),
+          randint(0,1), randint(0,1), randint(0,1), randint(0,1), 
+          randint(0,1), randint(0,1), randint(0,1), randint(0,1)
+                   )
+                )
+         n[-1].draw()
+
 #cv2.ellipse(cnv, (330,330), (30,30), 0,  0, -90, (0, 0,255), 4)
 #cv2.ellipse(cnv, (330,330), (30,30), 0,  225, 270, (250, 200,30), 4)
+
+d = Dot(300, 300)
+cv2.namedWindow('TRAINS')
+#cv2.setMouseCallback('TRAINS', set_cross)
+cv2.setMouseCallback('TRAINS', d.set_cross)
 while True:
     cv2.imshow('TRAINS', cnv)
     key = cv2.waitKey(1)
